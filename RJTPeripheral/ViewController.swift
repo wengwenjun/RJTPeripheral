847b4a505f30908049fe7c01579dc663e640fc17
Mohit Athwani <mohit@athwani.net>
2017-05-15 13:47:33 -0700

    Initial Commit

---
+++ RJTPeripheral/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RJTPeripheral
+//
+//  Created by Labs on 5/15/17.
+//  Copyright © 2017 Tera Mo Labs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
